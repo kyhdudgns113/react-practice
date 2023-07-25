acf933ecb408adeb0c1cb142ac7b0920d5f8688d
kyhdudgns113 <dudgns113@gmail.com>
2023-07-26 04:04:36 +0900

    230726 04:05 Finish 03-4

--- ch03/ch03_4/src/App.tsx
+++ ch03/ch03_4/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DirectionTest from './pages/DirectionTest'
+import WrapTest from './pages/WrapTest'
+import MinMaxTest from './pages/MinMaxTest'
+import JustifyCenterTest from './pages/JustifyCenterTest'
+import AlignTest from './pages/AlignTest'
+import UserContainer from './pages/UserContainer'
+import CardContainer from './pages/CardContainer'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <CardContainer />
+      <UserContainer />
+      <AlignTest />
+      <JustifyCenterTest />
+      <MinMaxTest />
+      <WrapTest />
+      <DirectionTest />
+    </main>
+  )
 }
-
-export default App;
