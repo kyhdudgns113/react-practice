0c2f77220acf9e5929d7e34617f7f11541fc77dc
kyhdudgns113 <dudgns113@gmail.com>
2023-07-27 03:38:03 +0900

    230727 03:35 Finish 04-4

--- ch04/ch04_4/src/App.tsx
+++ ch04/ch04_4/src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ClassLifecycle from './pages/ClassLifecycle'
+import WindowResizeTest from './pages/WindowResizeTest'
+import FetchTest from './pages/FetchTest'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <FetchTest />
+      <WindowResizeTest />
+      <ClassLifecycle />
+    </main>
+  )
 }
-
-export default App;
