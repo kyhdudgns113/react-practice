7a0739b2c6c58622337192a28f9b55341c7187eb
kyhdudgns113 <dudgns113@gmail.com>
2023-07-29 03:10:43 +0900

    230728 20:35 Finish 05-3

---
+++ ch05/ch05_3/src/pages/LoadingTest.tsx
@@ -0,0 +1,37 @@
+import {useCallback} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import type {AppState} from '../store'
+import {Title} from '../components'
+import {Button} from '../theme/daisyui'
+import * as L from '../store/loading'
+import {useStore} from '../store'
+
+export default function LoadingTest() {
+  const dispatch = useDispatch()
+  const loading = useSelector<AppState, L.State>(({loading}) => loading)
+  const gkd = useSelector<AppState, number>(({gkd}) => gkd)
+
+  const doTimedLoading = useCallback(() => {
+    dispatch<any>(L.doTimedLoading(1000))
+  }, [dispatch])
+
+  return (
+    <section className="mt-4">
+      <Title>LoadingTest</Title>
+      <div className="mt-4"></div>
+      <div className="flex justify-center mt-4">
+        <Button
+          className="btn-sm btn-primary"
+          onClick={doTimedLoading}
+          disabled={loading}>
+          do timed loading
+        </Button>
+      </div>
+      {loading && (
+        <div className="flex items-center justify-center">
+          <button className="btn btn-circle loading"></button>
+        </div>
+      )}
+    </section>
+  )
+}
